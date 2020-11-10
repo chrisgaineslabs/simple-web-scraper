43e0b92e228dd676ff332c17c34b47239184c76c
Keyser Soze <christopherjgaines@gmail.com>
2020-11-10 17:46:41 -0500

    extract select stats

--- nfl_stats.py
+++ nfl_stats.py
@@ -4,7 +4,18 @@ def initialize():
 	browser = webdriver.Chrome(executable_path='/usr/local/bin/chromedriver')
 	response = browser.get('https://www.nfl.com/stats/player-stats/')
 	print('on stats page')
-	zxcv
-
+	rows = browser.find_elements_by_xpath("//*[@id='main-content']/section[3]/div/div/div/div/table/tbody/tr")	
+	for row_num in range(1,len(rows) + 1):
+		row_xpath = "//*[@id='main-content']/section[3]/div/div/div/div/table/tbody/tr[{}]".format(row_num)
+		quarterBack = browser.find_element_by_xpath(row_xpath + '/td[1]').text
+		passYds = browser.find_element_by_xpath(row_xpath + '/td[2]').text
+		ydsPerAtt = browser.find_element_by_xpath(row_xpath + '/td[3]').text
+		numAtts = browser.find_element_by_xpath(row_xpath + '/td[4]').text
+		Cmp = browser.find_element_by_xpath(row_xpath + '/td[5]').text
+		CmpPercent = browser.find_element_by_xpath(row_xpath + '/td[6]').text
+		TD = browser.find_element_by_xpath(row_xpath + '/td[7]').text
+		INT = browser.find_element_by_xpath(row_xpath + '/td[8]').text
+		print(quarterBack, passYds, ydsPerAtt, numAtts, Cmp, CmpPercent, TD, INT)
 
 initialize()
+
